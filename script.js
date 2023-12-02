c665d61b3cae7aa9e35e881155e4ee99578dee1c
Adhyatma Dwivedi <adhyatma.d01@gmail.com>
2023-12-02 18:48:00 +0530

    added one dropdown div

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $('.q1 img').click(function(){
-    $('.a').slideDown("slow");
+    $('.a1').slideDown("slow");
     $('.q1 .mns').show()
     $(this).hide();
 });
 
 $('.q1 .mns').click(function(){
-    $('.a').slideUp("slow");
+    $('.a1').slideUp("slow");
     $('.q1 img').show();
     $(this).hide();
 });
